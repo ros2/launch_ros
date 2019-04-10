87350dcbaf6d1604a8cdcf5d9bf78bf204fbbf87
Michel Hidalgo <michel@ekumenlabs.com>
2019-04-10 15:52:17 -0300

    Update talker/listener example test.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- launch_testing_ros/examples/talker_listener.test.py
+++ launch_testing_ros/examples/talker_listener.test.py
@@ -85,7 +85,7 @@ class TestTalkerListenerLink(unittest.TestCase):
             try:
                 publisher.publish(msg)
                 proc_output.assertWaitFor(
-                    msg=msg.data,
+                    expected_output=msg.data,
                     process=listener,
                     timeout=1.0
                 )
@@ -132,7 +132,7 @@ class TestTalkerListenerLink(unittest.TestCase):
         # Make sure the talker also output the same data via stdout
         for txt in [msg.data for msg in msgs_rx]:
             self.proc_output.assertWaitFor(
-                msg=txt,
+                expected_output=txt,
                 process=talker
             )
 
@@ -151,7 +151,7 @@ class TestTalkerListenerLink(unittest.TestCase):
 
             pub.publish(msg)
             self.proc_output.assertWaitFor(
-                msg=msg.data,
+                expected_output=msg.data,
                 process=listener
             )
 

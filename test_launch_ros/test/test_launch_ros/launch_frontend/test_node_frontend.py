0608922be4cd5ba71462442b1844892d71ab9db5
ivanpauno <ivanpauno@ekumenlabs.com>
2019-07-02 17:07:21 -0300

    Updated old import in test
    Signed-off-by: ivanpauno <ivanpauno@ekumenlabs.com>

--- test_launch_ros/test/test_launch_ros/launch_frontend/test_node_frontend.py
+++ test_launch_ros/test/test_launch_ros/launch_frontend/test_node_frontend.py
@@ -18,8 +18,7 @@ import io
 import textwrap
 
 from launch import LaunchService
-
-from launch_frontend import Parser
+from launch.launch_frontend import Parser
 
 from launch_ros.utilities import evaluate_parameters
 

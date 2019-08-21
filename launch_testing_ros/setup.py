ef7474c6267409feec23325fa2d54fd2836a818b
Michel Hidalgo <michel@ekumenlabs.com>
2019-08-21 15:19:36 -0300

    Install examples properly.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- launch_testing_ros/setup.py
+++ launch_testing_ros/setup.py
@@ -10,7 +10,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/launch_testing_ros']),
         ('lib/launch_testing_ros', glob.glob('example_nodes/**')),
-        ('share/launch_testing_ros/examples', glob.glob('examples/[!_]**')),
+        ('share/launch_testing_ros/examples', glob.glob('test/examples/[!_]**')),
     ],
     entry_points={
         'pytest11': ['launch_ros = launch_testing_ros.pytest.hooks'],

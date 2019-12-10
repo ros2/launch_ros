e066402266b3819ae15401c9081823f829970772
William Woodall <william@osrfoundation.org>
2019-12-10 14:11:45 -0800

    check for shutdown while waiting for a service response to avoid hang during shutdown
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- launch_ros/launch_ros/actions/lifecycle_node.py
+++ launch_ros/launch_ros/actions/lifecycle_node.py
@@ -15,6 +15,7 @@
 """Module for the LifecycleNode action."""
 
 import functools
+import threading
 from typing import cast
 from typing import List
 from typing import Optional
@@ -82,7 +83,30 @@ class LifecycleNode(Node):
                         self.__rclpy_change_state_client.srv_name),
                 )
                 return
-        response = self.__rclpy_change_state_client.call(request)
+
+        # Asynchronously wait so that we can periodically check for shutdown.
+        event = threading.Event()
+
+        def unblock(future):
+            nonlocal event
+            event.set()
+
+        response_future = self.__rclpy_change_state_client.call_async(request)
+        response_future.add_done_callback(unblock)
+
+        while not event.wait(1.0):
+            if context.is_shutdown:
+                self.___logger.warning(
+                    "Abandoning wait for the '{}' service response, due to shutdown.".format(
+                        self.__rclpy_change_state_client.srv_name),
+                )
+                response_future.cancel()
+                return
+
+        if response_future.exception() is not None:
+            raise response_future.exception()
+        response = response_future.result()
+
         if not response.success:
             self.__logger.error(
                 "Failed to make transition '{}' for LifecycleNode '{}'".format(

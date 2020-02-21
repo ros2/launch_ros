b81028ab06ebb40b1b169637700f5d0ad07f4e59
Shane Loretz <sloretz@osrfoundation.org>
2020-02-20 17:43:55 -0800

    Typo in comment
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ros2launch/ros2launch/command/launch.py
+++ ros2launch/ros2launch/command/launch.py
@@ -62,7 +62,7 @@ def package_name_or_launch_file_completer(prefix, parsed_args, **kwargs):
         # complete launch files in a directory
         for launch_file in get_launch_file_paths(path=dirname):
             if os.path.normpath(os.path.dirname(launch_file)) == os.path.normpath(dirname):
-                # Get launch_file_paths is recursive; complete only launch files in first level
+                # get_launch_file_paths() is recursive; complete only launch files in first level
                 completions.append(prepend + os.path.basename(launch_file))
 
         # complete directories since they may contain launch files

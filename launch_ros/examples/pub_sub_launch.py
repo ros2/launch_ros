d0bee1b098e584da69a060f8a8ddc8428bd8e34e
Michel Hidalgo <michel@ekumenlabs.com>
2020-05-13 14:06:35 -0300

    Fix new flake8 errors.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- launch_ros/examples/pub_sub_launch.py
+++ launch_ros/examples/pub_sub_launch.py
@@ -19,11 +19,11 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'launch'))  # noqa
 
-from launch import LaunchDescription
-from launch import LaunchIntrospector
-from launch import LaunchService
+from launch import LaunchDescription  # noqa: E402
+from launch import LaunchIntrospector  # noqa: E402
+from launch import LaunchService  # noqa: E402
 
-import launch_ros.actions
+import launch_ros.actions  # noqa: E402
 
 
 def main(argv=sys.argv[1:]):

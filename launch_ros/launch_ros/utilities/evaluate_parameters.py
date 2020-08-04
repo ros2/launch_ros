c98e16bf127d9e64fa0be81ae43319abef952344
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-08-04 18:04:21 -0300

    Fix problems when parsing a `Command` `Substitution` as a parameter value (#137)
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- launch_ros/launch_ros/utilities/evaluate_parameters.py
+++ launch_ros/launch_ros/utilities/evaluate_parameters.py
@@ -31,6 +31,8 @@ from launch.utilities import perform_substitutions
 
 import yaml
 
+from ..parameter_descriptions import Parameter as ParameterDescription
+from ..parameter_descriptions import ParameterValue as ParameterValueDescription
 from ..parameters_type import EvaluatedParameters
 from ..parameters_type import EvaluatedParameterValue
 from ..parameters_type import Parameters
@@ -113,6 +115,8 @@ def evaluate_parameter_dict(
                 for i, subvalue in enumerate(value):
                     output_value.append(target_type(subvalue))
                     evaluated_value = tuple(output_value)
+        elif isinstance(value, ParameterValueDescription):
+            evaluated_value = value.evaluate(context)
         else:
             # Value is a singular type, so nothing to evaluate
             ensure_argument_type(value, (float, int, str, bool, bytes), 'value')
@@ -140,6 +144,8 @@ def evaluate_parameters(context: LaunchContext, parameters: Parameters) -> Evalu
         if isinstance(param, tuple) and len(param) and isinstance(param[0], Substitution):
             # Evaluate a list of Substitution to a file path
             output_params.append(pathlib.Path(perform_substitutions(context, list(param))))
+        elif isinstance(param, ParameterDescription):
+            output_params.append(param)
         elif isinstance(param, Mapping):
             # It's a list of name/value pairs
             output_params.append(evaluate_parameter_dict(context, param))
--- test_launch_ros/test/test_launch_ros/frontend/test_node_frontend.py
+++ test_launch_ros/test/test_launch_ros/frontend/test_node_frontend.py
@@ -30,12 +30,13 @@ yaml_params = yaml_params.replace('\\', '\\\\')
 python_executable = sys.executable.replace('\\', '\\\\')
 
 
-def test_launch_remapping_xml():
+def test_launch_frontend_xml():
     xml_file = textwrap.dedent(
         r"""
         <launch>
             <let name="a_string" value="\'[2, 5, 8]\'"/>
             <let name="a_list" value="[2, 5, 8]"/>
+            <let name="my_value" value="100"/>
             <node pkg="demo_nodes_py" exec="talker_qos" output="screen" name="my_talker" namespace="my_ns" exec_name="my_talker_process" args="--number_of_cycles 1">
                 <param name="param1" value="ads"/>
                 <param name="param_group1">
@@ -47,13 +48,14 @@ def test_launch_remapping_xml():
                     <param name="param5" value="$(var a_string)"/>
                     <param name="param6" value="2., 5., 8." value-sep=", "/>
                     <param name="param7" value="'2', '5', '8'" value-sep=", "/>
-                    <!-- TODO(hidmic): revisit after https://github.com/ros2/launch_ros/pull/137 gets merged -->
                     <param name="param8" value="&quot;'2'&quot;, &quot;'5'&quot;, &quot;'8'&quot;" value-sep=", "/>
                     <param name="param9" value="\'2\', \'5\', \'8\'" value-sep=", "/>
-                    <!-- TODO(hidmic): revisit after https://github.com/ros2/launch_ros/pull/137 gets merged -->
                     <param name="param10" value="&quot;'asd'&quot;, &quot;'bsd'&quot;, &quot;'csd'&quot;" value-sep=", "/>
                     <param name="param11" value="'\asd', '\bsd', '\csd'" value-sep=", "/>
-                    <param name="param12" value=""/>
+                    <param name="param12" value="''"/>
+                    <param name="param13" value="$(var my_value)" type="str"/>
+                    <param name="param14" value="'2', '5', '8'" value-sep=", " type="list_of_str"/>
+                    <param name="param15" value="2, 5, 8" value-sep=", " type="list_of_str"/>
                 </param>
                 <param from="{}"/>
                 <env name="var" value="1"/>
@@ -65,10 +67,10 @@ def test_launch_remapping_xml():
         """.format(yaml_params, python_executable))  # noqa: E501
 
     with io.StringIO(xml_file) as f:
-        check_launch_remapping(f)
+        check_launch_node(f)
 
 
-def test_launch_remapping_yaml():
+def test_launch_frontend_yaml():
     yaml_file = textwrap.dedent(
         r"""
         launch:
@@ -78,6 +80,9 @@ def test_launch_remapping_yaml():
             - let:
                 name: 'a_list'
                 value: '[2, 5, 8]'
+            - let:
+                name: 'my_value'
+                value: '100'
             - node:
                 pkg: demo_nodes_py
                 exec: talker_qos
@@ -115,6 +120,15 @@ def test_launch_remapping_yaml():
                             value: ['\asd', '\bsd', '\csd']
                         -   name: param12
                             value: ''
+                        -   name: param13
+                            value: '$(var my_value)'
+                            type: str
+                        -   name: param14
+                            value: ["'2'", "'5'", "'8'"]
+                            type: list_of_str
+                        -   name: param15
+                            value: ['2', '5', '8']
+                            type: list_of_str
                     -   from: {}
                 env:
                     -   name: var
@@ -133,10 +147,10 @@ def test_launch_remapping_yaml():
         """.format(yaml_params, python_executable))  # noqa: E501
 
     with io.StringIO(yaml_file) as f:
-        check_launch_remapping(f)
+        check_launch_node(f)
 
 
-def check_launch_remapping(file):
+def check_launch_node(file):
     root_entity, parser = Parser.load(file)
     ld = parser.parse_description(root_entity)
     ls = LaunchService()
@@ -144,8 +158,9 @@ def check_launch_remapping(file):
     assert(0 == ls.run())
     evaluated_parameters = evaluate_parameters(
         ls.context,
-        ld.describe_sub_entities()[2]._Node__parameters
+        ld.describe_sub_entities()[3]._Node__parameters
     )
+    assert len(evaluated_parameters) == 3
     assert isinstance(evaluated_parameters[0], dict)
     assert isinstance(evaluated_parameters[1], dict)
     assert isinstance(evaluated_parameters[2], pathlib.Path)
@@ -164,24 +179,31 @@ def check_launch_remapping(file):
     assert 'param_group1.param9' in param_dict
     assert 'param_group1.param10' in param_dict
     assert 'param_group1.param11' in param_dict
+    assert 'param_group1.param12' in param_dict
+    assert 'param_group1.param13' in param_dict
+    assert 'param_group1.param14' in param_dict
+    assert 'param_group1.param15' in param_dict
     assert param_dict['param_group1.param_group2.param2'] == 2
-    assert param_dict['param_group1.param3'] == (2, 5, 8)
-    assert param_dict['param_group1.param4'] == (2, 5, 8)
+    assert param_dict['param_group1.param3'] == [2, 5, 8]
+    assert param_dict['param_group1.param4'] == [2, 5, 8]
     assert param_dict['param_group1.param5'] == '[2, 5, 8]'
-    assert param_dict['param_group1.param6'] == (2., 5., 8.)
-    assert param_dict['param_group1.param7'] == ('2', '5', '8')
-    assert param_dict['param_group1.param8'] == ("'2'", "'5'", "'8'")
-    assert param_dict['param_group1.param9'] == ("'2'", "'5'", "'8'")
-    assert param_dict['param_group1.param10'] == ("'asd'", "'bsd'", "'csd'")
-    assert param_dict['param_group1.param11'] == ('asd', 'bsd', 'csd')
+    assert param_dict['param_group1.param6'] == [2., 5., 8.]
+    assert param_dict['param_group1.param7'] == ['2', '5', '8']
+    assert param_dict['param_group1.param8'] == ["'2'", "'5'", "'8'"]
+    assert param_dict['param_group1.param9'] == ["'2'", "'5'", "'8'"]
+    assert param_dict['param_group1.param10'] == ["'asd'", "'bsd'", "'csd'"]
+    assert param_dict['param_group1.param11'] == ['asd', 'bsd', 'csd']
     assert param_dict['param_group1.param12'] == ''
+    assert param_dict['param_group1.param13'] == '100'
+    assert param_dict['param_group1.param14'] == ["'2'", "'5'", "'8'"]
+    assert param_dict['param_group1.param15'] == ['2', '5', '8']
 
     # Check remappings exist
-    remappings = ld.describe_sub_entities()[2]._Node__remappings
+    remappings = ld.describe_sub_entities()[3]._Node__remappings
     assert remappings is not None
     assert len(remappings) == 2
 
-    listener_node_action = ld.describe_sub_entities()[3]
+    listener_node_action = ld.describe_sub_entities()[4]
     listener_node_cmd = listener_node_action.process_details['cmd']
     assert [
         sys.executable, '-c', 'import sys; print(sys.argv[1:])'

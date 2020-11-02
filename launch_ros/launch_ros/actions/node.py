e3a97b278ceea4e3e74c61e229a317068bf10ae4
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-11-02 18:26:33 -0300

    Validate complex attributes of 'node' action
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- launch_ros/launch_ros/actions/node.py
+++ launch_ros/launch_ros/actions/node.py
@@ -192,6 +192,7 @@ class Node(ExecuteProcess):
                     data_type = get_data_type_from_identifier(type_identifier)
                 value = param.get_attr('value', data_type=data_type, optional=True)
                 nested_params = param.get_attr('param', data_type=List[Entity], optional=True)
+                param.assert_entity_completely_parsed()
                 if value is not None and nested_params:
                     raise RuntimeError(
                         'nested parameters and value attributes are mutually exclusive')
@@ -224,8 +225,9 @@ class Node(ExecuteProcess):
                     allow_substs = parser.parse_substitution(allow_substs)
                 else:
                     allow_substs = bool(allow_substs)
+                param.assert_entity_completely_parsed()
                 normalized_params.append(
-                    ParameterFile(parser.parse_substitution(from_attr), allow_substs=allow_substs))
+                    ParameterFile(parser.parse_substitution(from_attr), allow_substs=allow_substs)
                 continue
             elif name is not None:
                 if allow_substs is not None:
@@ -269,6 +271,8 @@ class Node(ExecuteProcess):
                     parser.parse_substitution(remap.get_attr('to'))
                 ) for remap in remappings
             ]
+            for remap in remappings:
+                remap.assert_entity_completely_parsed()
         parameters = entity.get_attr('param', data_type=List[Entity], optional=True)
         if parameters is not None:
             kwargs['parameters'] = cls.parse_nested_parameters(parameters, parser)

5b74c70aa9fafd1e8e57c216febfe7c3dbf7041f
Chris Lalancette <clalancette@openrobotics.org>
2021-04-07 13:51:29 +0000

    Add a package marker to test_launch_ros.
    This quiets the warning from colcon about it not explicitly
    installing a marker.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- test_launch_ros/setup.py
+++ test_launch_ros/setup.py
@@ -16,6 +16,8 @@ setup(
     zip_safe=True,
     data_files=[
         ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
     ],
     author='William Woodall',
     author_email='william@osrfoundation.org',

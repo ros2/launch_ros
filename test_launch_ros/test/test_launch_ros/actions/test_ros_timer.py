d44d2f1b3bb7a4c51ddfd720ec85a9eb38297b00
Rebecca Butler <rebecca@openrobotics.org>
2021-06-11 11:28:37 -0400

    Improve accuracy of measuring timeout in tests
    Signed-off-by: Rebecca Butler <rebecca@openrobotics.org>

--- test_launch_ros/test/test_launch_ros/actions/test_ros_timer.py
+++ test_launch_ros/test/test_launch_ros/actions/test_ros_timer.py
@@ -31,6 +31,28 @@ from rosgraph_msgs.msg import Clock as ClockMsg
 from builtin_interfaces.msg import Time
 
 
+start_time = None
+end_time = None
+
+
+def set_start_time(context):
+    global start_time
+    start_time = time.time()
+
+
+def set_end_time(context):
+    global end_time
+    end_time = time.time()
+
+
+def _shutdown_listener_factory(reasons_arr):
+    return launch.actions.RegisterEventHandler(
+        launch.event_handlers.OnShutdown(
+            on_shutdown=lambda event, context: reasons_arr.append(event)
+        )
+    )
+
+
 def test_multiple_launch_with_timers():
     def generate_launch_description():
         return launch.LaunchDescription([
@@ -52,14 +74,6 @@ def test_multiple_launch_with_timers():
     assert 0 == ls.run()
 
 
-def _shutdown_listener_factory(reasons_arr):
-    return launch.actions.RegisterEventHandler(
-        launch.event_handlers.OnShutdown(
-            on_shutdown=lambda event, context: reasons_arr.append(event)
-        )
-    )
-
-
 def test_timer_action_sanity_check():
     """Test that timer actions work (sanity check)."""
     # This test is structured like test_shutdown_preempts_timers
@@ -69,9 +83,12 @@ def test_timer_action_sanity_check():
 
     ld = launch.LaunchDescription([
 
+        launch.actions.OpaqueFunction(function=set_start_time),
+
         RosTimer(
             period=1.,
             actions=[
+                launch.actions.OpaqueFunction(function=set_end_time),
                 launch.actions.Shutdown(reason='One second timeout')
             ]
         ),
@@ -79,15 +96,15 @@ def test_timer_action_sanity_check():
         _shutdown_listener_factory(shutdown_reasons),
     ])
 
-    start_time = time.time()
     ls = launch.LaunchService()
     ls.include_launch_description(ld)
     assert 0 == ls.run()
-
-    # Verify that ~1 sec has passed between start of test and shutdown
-    assert (time.time() - start_time) > 1
-
     assert shutdown_reasons[0].reason == 'One second timeout'
+
+    # Verify that 1 sec has passed between start of test and timeout
+    tolerance = 0.1
+    assert (end_time - start_time) > 1 - tolerance
+    assert (end_time - start_time) < 1 + tolerance
 
 
 def test_shutdown_preempts_timers():
@@ -141,22 +158,23 @@ def test_timer_uses_sim_time():
 
     ld = launch.LaunchDescription([
 
+        launch.actions.OpaqueFunction(function=set_start_time),
+
         RosTimer(
             period=200.,
             actions=[
-                launch.actions.Shutdown(reason='timer expired')
+                launch.actions.OpaqueFunction(function=set_end_time)
             ],
         ),
 
         SetUseSimTime(True)  # Must be set to allow timer action to use sim time
     ])
 
-    start_time = time.time()
-
     ls = launch.LaunchService()
     ls.include_launch_description(ld)
     assert 0 == ls.run()
 
     # Simulated time is 100x faster, so 200 sec timer should finish in 2 sec
-    # (Some extra time provided)
-    assert (time.time() - start_time < 6)
+    tolerance = 0.1
+    assert (end_time - start_time) > 2 - tolerance
+    assert (end_time - start_time) < 2 + tolerance

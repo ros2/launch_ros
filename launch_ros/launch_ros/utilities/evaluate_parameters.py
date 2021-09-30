61574cdf3442a72f0b967952c1c7d4a3921c0de9
David V. Lu <davidvlu@gmail.com>
2021-09-30 11:23:19 -0400

    Update YAML parsing logic
    Signed-off-by: David V. Lu <davidvlu@gmail.com>

--- launch_ros/launch_ros/utilities/evaluate_parameters.py
+++ launch_ros/launch_ros/utilities/evaluate_parameters.py
@@ -70,9 +70,12 @@ def evaluate_parameter_dict(
             if isinstance(value[0], Substitution):
                 # Value is a list of substitutions, so perform them to make a string
                 evaluated_value = perform_substitutions(context, list(value))
-                yaml_evaluated_value = yaml.safe_load(evaluated_value)
-                if yaml_evaluated_value is None:
-                    yaml_evaluated_value = ''
+
+                try:
+                    yaml_evaluated_value = yaml.safe_load(evaluated_value)
+                except yaml.YAMLError:
+                    yaml_evaluated_value = None
+
                 if type(yaml_evaluated_value) in (bool, int, float, str, bytes):
                     evaluated_value = yaml_evaluated_value
                 elif isinstance(yaml_evaluated_value, Sequence):
@@ -85,12 +88,8 @@ def evaluate_parameter_dict(
                         )
                     evaluated_value = tuple(yaml_evaluated_value)
                 else:
-                    raise TypeError(
-                        'Allowed value types are bytes, bool, int, float, str, Sequence[bool]'
-                        ', Sequence[int], Sequence[float], Sequence[str]. Got {}.'.format(
-                            type(yaml_evaluated_value)
-                        )
-                    )
+                    # If the yaml does not evaluate to a valid parameter, then save as a string
+                    pass
             elif isinstance(value[0], Sequence):
                 # Value is an array of a list of substitutions
                 output_subvalue: List[str] = []

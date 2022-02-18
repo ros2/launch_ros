b9531dd4a0f267d5e545b23725dc90f7f08f430a
matthew.lanting <mlanting@dcscorp.com>
2022-02-18 10:53:28 -0500

    removed license language to match aggreed upon format
    Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    
    Signed-off-by: matthew.lanting <mlanting@dcscorp.com>

--- launch_ros/launch_ros/descriptions/node_trait.py
+++ launch_ros/launch_ros/descriptions/node_trait.py
@@ -11,14 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# DISTRIBUTION A. Approved for public release; distribution unlimited.
-# OPSEC #4584.
-#
-# Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
-# Part 252.227-7013 or 7014 (Feb 2014).
-#
-# This notice must appear in all copies of this file and its derivatives.
 
 """Module for a description of a NodeTrait."""
 

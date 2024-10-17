14c10957430454ede17a027113a322e9f0f56471
Tomoya Fujita <Tomoya.Fujita@sony.com>
2024-10-17 09:35:01 -0700

    [Backport] Make launch_testing_ros examples more robust. (#394) (#420)
    * Make launch_testing_ros examples more robust. (#394)
    
    The original goal here was to expand the timeouts on
    the launch_testing_ros examples so that they are more
    robust.  I did that, and in most cases these tests now
    wait at least 15 seconds for things to happen.
    
    Along the way, I also somewhat rewrote some
    of these tests so they properly clean up after themselves.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- launch_testing_ros/test/examples/check_node_launch_test.py
+++ launch_testing_ros/test/examples/check_node_launch_test.py
@@ -24,7 +24,6 @@ import launch_testing.actions
 import launch_testing.markers
 import pytest
 import rclpy
-from rclpy.node import Node
 
 
 @pytest.mark.launch_test
@@ -49,26 +48,23 @@ def generate_test_description():
 
 class TestFixture(unittest.TestCase):
 
-    def test_node_start(self, proc_output):
+    def setUp(self):
         rclpy.init()
-        try:
-            node = MakeTestNode('test_node')
-            assert node.wait_for_node('demo_node_1', 8.0), 'Node not found !'
-        finally:
-            rclpy.shutdown()
+        self.node = rclpy.create_node('test_node')
 
+    def tearDown(self):
+        self.node.destroy_node()
+        rclpy.shutdown()
 
-class MakeTestNode(Node):
-
-    def __init__(self, name='test_node'):
-        super().__init__(name)
-
-    def wait_for_node(self, node_name, timeout=8.0):
-        start = time.time()
-        flag = False
+    def test_node_start(self, proc_output):
+        found = False
         print('Waiting for node...')
-        while time.time() - start < timeout and not flag:
-            flag = node_name in self.get_node_names()
+        # demo_node_1 won't start for at least 5 seconds after this test
+        # is launched, so we wait for a total of up to 20 seconds for it
+        # to appear.
+        start = time.time()
+        while time.time() - start < 20.0 and not found:
+            found = 'demo_node_1' in self.node.get_node_names()
             time.sleep(0.1)
 
-        return flag
+        assert found, 'Node not found!'

e1a6d777217ee4dd5a0b6ad68b2f65e6701a5658
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-02-05 12:58:28 -0800

    add yaml dumper representator for str type to keep quotes always. (#436)
    * add yaml dumper representator for str type to keep quotes always.
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    
    * representator should be applied to conversion from py dictionary.
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    
    ---------
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- launch_ros/launch_ros/actions/node.py
+++ launch_ros/launch_ros/actions/node.py
@@ -373,6 +373,10 @@ class Node(ExecuteProcess):
                 self.node_name if self.is_node_name_fully_specified() else '/**':
                 {'ros__parameters': params}
             }
+
+            def quoted_representor(dumper, data):
+                return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+            yaml.add_representer(str, quoted_representor)
             yaml.dump(param_dict, h, default_flow_style=False)
             return param_file_path
 

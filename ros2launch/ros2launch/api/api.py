10229af9bfe6867b1dcedf1c4ee92d612ac03b34
Tanishq Chaudhary <tanishqchaudhary101010@gmail.com>
2025-04-30 21:11:29 +0530

    improve readability for accessing the log_file_name
    Co-authored-by: Katherine Scott <katherineAScott@gmail.com>
    Signed-off-by: Tanishq Chaudhary <tanishqchaudhary101010@gmail.com>

--- ros2launch/ros2launch/api/api.py
+++ ros2launch/ros2launch/api/api.py
@@ -159,7 +159,9 @@ def launch_a_launch_file(
     if args and args.launch_prefix_filter:
         launch_file_arguments.append(f'launch-prefix-filter:={args.launch_prefix_filter}')
 
-    log_file_name = args.log_file_name if args and args.log_file_name else 'launch'
+    log_file_name = 'launch'
+    if args and args.log_file_name:
+        log_file_name = args.log_file_name
     launch_service = launch.LaunchService(
         argv=launch_file_arguments,
         noninteractive=noninteractive,

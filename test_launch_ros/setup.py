9c79cba2bb79808d9a892b95fc72128a677cd4c2
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-17 11:37:10 +0200

    0.29.3

--- test_launch_ros/setup.py
+++ test_launch_ros/setup.py
@@ -5,7 +5,7 @@ package_name = 'test_launch_ros'
 
 setup(
     name=package_name,
-    version='0.29.2',
+    version='0.29.3',
     packages=find_packages(exclude=['test']),
     install_requires=[
         'setuptools',

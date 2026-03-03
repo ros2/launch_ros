ac51372fdab20ee675cb3619e2df04797c180bb3
Nathan Wiebe Neufeldt <wn.nathan@gmail.com>
2026-03-02 22:20:22 -0500

    Fix linting failures
    Signed-off-by: Nathan Wiebe Neufeldt <wn.nathan@gmail.com>

--- launch_ros/launch_ros/substitutions/ament_index.py
+++ launch_ros/launch_ros/substitutions/ament_index.py
@@ -14,18 +14,19 @@
 
 """Module for the AmentIndex substitution."""
 
+from typing import Any, Dict, List, Sequence, Text, Tuple, Type
+
 from ament_index_python.resources import get_resource
 
 from launch.frontend import expose_substitution
 from launch.launch_context import LaunchContext
 from launch.some_substitutions_type import SomeSubstitutionsType
-from launch.substitutions import Substitution, PathSubstitution
+from launch.substitution import Substitution
+from launch.substitutions import PathSubstitution
 from launch.utilities import normalize_to_list_of_substitutions, perform_substitutions
 
-from typing import Sequence, Tuple, Type, Dict, Any, List, Text
 
-
-@expose_substitution("ament-index-resource")
+@expose_substitution('ament-index-resource')
 class AmentIndexResource(PathSubstitution):
     """
     Substitution that looks up the path for the given resource type and name.
@@ -35,46 +36,46 @@ class AmentIndexResource(PathSubstitution):
 
     def __init__(
         self,
-        type: SomeSubstitutionsType,
-        name: SomeSubstitutionsType,
+        resource_type: SomeSubstitutionsType,
+        resource_name: SomeSubstitutionsType,
     ) -> None:
         """Create an AmentIndexResource substitution."""
         super().__init__(self)
 
-        self.__type = normalize_to_list_of_substitutions(type)
-        self.__name = normalize_to_list_of_substitutions(name)
+        self.__type = normalize_to_list_of_substitutions(resource_type)
+        self.__name = normalize_to_list_of_substitutions(resource_name)
 
     @classmethod
     def parse(
         cls, data: Sequence[SomeSubstitutionsType]
-    ) -> Tuple[Type["AmentIndexResource"], Dict[str, Any]]:
+    ) -> Tuple[Type['AmentIndexResource'], Dict[str, Any]]:
         """Parse an AmentIndexResource subtitution."""
         if len(data) != 2:
-            raise TypeError("ament-index-resource expects 2 arguments")
+            raise TypeError('ament-index-resource expects 2 arguments')
         kwargs = {
-            "type": data[0],
-            "name": data[1],
+            'resource_type': data[0],
+            'resource_name': data[1],
         }
         return cls, kwargs
 
     @property
-    def type(self) -> List[Substitution]:
-        """Getter for type."""
+    def resource_type(self) -> List[Substitution]:
+        """Getter for resource_type."""
         return self.__type
 
     @property
-    def name(self) -> List[Substitution]:
-        """Getter for name."""
+    def resource_name(self) -> List[Substitution]:
+        """Getter for resource_name."""
         return self.__name
 
     def describe(self) -> Text:
         """Return a description of this substitution as a string."""
-        type_str = " + ".join(sub.describe() for sub in self.type)
-        name_str = " + ".join(sub.describe() for sub in self.name)
-        return f"AmentIndexResource({type_str}, {name_str})"
+        type_str = ' + '.join(sub.describe() for sub in self.resource_type)
+        name_str = ' + '.join(sub.describe() for sub in self.resource_name)
+        return f'AmentIndexResource({type_str}, {name_str})'
 
     def perform(self, context: LaunchContext) -> Text:
         """Perform the substitution by looking up the resource in the ament index."""
-        type = perform_substitutions(context, self.type)
-        name = perform_substitutions(context, self.name)
-        return get_resource(type, name)[1]
+        resource_type = perform_substitutions(context, self.resource_type)
+        resource_name = perform_substitutions(context, self.resource_name)
+        return get_resource(resource_type, resource_name)[1]
